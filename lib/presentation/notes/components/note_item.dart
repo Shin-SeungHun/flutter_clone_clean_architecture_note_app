6262d088efefa4bde7c102e081d56ae335961a28
shin-SeungHun <sng7387@gmail.com>
2024-02-28 15:59:05 +0900

    refactor: text style 수정

--- lib/presentation/notes/components/note_item.dart
+++ lib/presentation/notes/components/note_item.dart
@@ -30,14 +30,14 @@ class NoteItem extends StatelessWidget {
               children: [
                 Text(
                   note.title,
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  style: Theme.of(context).textTheme.headlineSmall,
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis,
                 ),
                 const SizedBox(height: 8),
                 Text(
                   note.content,
-                  style: Theme.of(context).textTheme.headlineSmall,
+                  style: Theme.of(context).textTheme.bodySmall,
                   maxLines: 3,
                   overflow: TextOverflow.ellipsis,
                 ),

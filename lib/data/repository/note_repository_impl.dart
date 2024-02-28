9a813db60b820fedbfb447ed4d40eac796742f4c
shin-SeungHun <sng7387@gmail.com>
2024-02-28 11:44:58 +0900

    feat: repository에 NoteDb 객체 생성

--- lib/data/repository/note_repository_impl.dart
+++ lib/data/repository/note_repository_impl.dart
@@ -1,7 +1,13 @@
+import 'package:flutter_clone_clean_architecture_note_app/data/data_source/note_db.dart';
 import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
 import 'package:flutter_clone_clean_architecture_note_app/domain/repository/note_repository.dart';
 
 class NoteRepositoryImpl implements NoteRepository {
+  final NoteDb db;
+  const NoteRepositoryImpl({
+    required this.db,
+  });
+
   @override
   Future<void> deleteNote({required Note note}) {
     // TODO: implement deleteNote

dc1d084b55b533e9faa7e0fbd306c584918aa602
shin-SeungHun <sng7387@gmail.com>
2024-02-28 14:16:10 +0900

    feat: notes_screen ui 작성

---
+++ lib/presentation/notes/components/note_item.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
+
+class NoteItem extends StatelessWidget {
+  final Note note;
+  final Function? onDeleteTap;
+
+  const NoteItem({
+    super.key,
+    required this.note,
+    this.onDeleteTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(note.color),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  note.title,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  note.content,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 1,
+            right: 1,
+            child: IconButton(
+              onPressed: () => onDeleteTap?.call(),
+              icon: const Icon(Icons.delete),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
+import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/components/note_item.dart';
+import 'package:flutter_clone_clean_architecture_note_app/ui/colors.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
@@ -7,9 +12,45 @@ class NotesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text(
+          'Your note',
+          style: TextStyle(fontSize: 30),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.sort),
+          ),
+        ],
+        elevation: 0,
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            NoteItem(
+              note: Note(
+                title: 'title',
+                content: 'content',
+                color: wisteria.value,
+                timestamp: 1,
+              ),
+            ),
+            NoteItem(
+              note: Note(
+                title: 'title',
+                content: 'content',
+                color: skyBlue.value,
+                timestamp: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

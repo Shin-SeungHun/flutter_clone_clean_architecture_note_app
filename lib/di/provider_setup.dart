20b803ac28114699b3a2a7fdd04009320d8f61ef
shin-SeungHun <sng7387@gmail.com>
2024-02-28 16:24:35 +0900

    feat: provider로 의존성 관리

---
+++ lib/di/provider_setup.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_clone_clean_architecture_note_app/data/data_source/note_db_helper.dart';
+import 'package:flutter_clone_clean_architecture_note_app/data/repository/note_repository_impl.dart';
+import 'package:flutter_clone_clean_architecture_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_clone_clean_architecture_note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  Database db = await openDatabase(
+    'notes.db',
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute(
+          'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
+    },
+  );
+
+  NoteDbHelper noteDbHelper = NoteDbHelper(db: db);
+  NoteRepository repository = NoteRepositoryImpl(db: noteDbHelper);
+  NotesViewModel notesViewModel = NotesViewModel(repository: repository);
+  AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository: repository);
+
+  return [
+    ChangeNotifierProvider(create: (_) => notesViewModel),
+    ChangeNotifierProvider(create: (_) => addEditNoteViewModel),
+  ];
+}

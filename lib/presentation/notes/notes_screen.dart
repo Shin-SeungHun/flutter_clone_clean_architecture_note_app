69700a203be9bb59ff7684547038f69042873e96
shin-SeungHun <sng7387@gmail.com>
2024-02-29 18:33:33 +0900

    refactor: 데이터를 보여주는 리스트뷰 작성, 데이터를 추가했을때와 그냥 뒤로가기 했을때 구분

--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
 import 'package:flutter_clone_clean_architecture_note_app/presentation/add_edit_note/add_edit_note_screen.dart';
 import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/components/note_item.dart';
-import 'package:flutter_clone_clean_architecture_note_app/ui/colors.dart';
+import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/notes_event.dart';
+import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final NotesViewModel viewModel = context.watch<NotesViewModel>();
+    final state = viewModel.state;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Your note',
-          style: TextStyle(fontSize: 30),
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+          ),
         ),
         actions: [
           IconButton(
@@ -24,35 +30,22 @@ class NotesScreen extends StatelessWidget {
         elevation: 0,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          bool? isSaved = await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const AddEditNoteScreen()),
           );
+
+          if (isSaved != null && isSaved) {
+            viewModel.onEvent(event: const NotesEvent.loadNotes());
+          }
         },
         child: const Icon(Icons.add),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView(
-          children: [
-            NoteItem(
-              note: Note(
-                title: 'title',
-                content: 'content',
-                color: wisteria.value,
-                timestamp: 1,
-              ),
-            ),
-            NoteItem(
-              note: Note(
-                title: 'title',
-                content: 'content',
-                color: skyBlue.value,
-                timestamp: 2,
-              ),
-            ),
-          ],
+          children: state.notes.map((note) => NoteItem(note: note)).toList(),
         ),
       ),
     );

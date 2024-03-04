1b9badf20a1ae417a1165ccfc38c535b8dba3a94
shin-SeungHun <sng7387@gmail.com>
2024-03-04 18:14:57 +0900

    refactor: viewModel에 상태적용

--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
 import 'package:flutter_clone_clean_architecture_note_app/domain/use_case/use_cases.dart';
+import 'package:flutter_clone_clean_architecture_note_app/domain/util/note_order.dart';
+import 'package:flutter_clone_clean_architecture_note_app/domain/util/order_type.dart';
 import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/notes_event.dart';
 import 'package:flutter_clone_clean_architecture_note_app/presentation/notes/notes_state.dart';
 
@@ -11,7 +13,11 @@ class NotesViewModel extends ChangeNotifier {
     _loadNotes();
   }
 
-  NotesState _state = const NotesState(notes: []);
+  NotesState _state = NotesState(
+      notes: [],
+      noteOrder: const NoteOrder.date(
+        orderType: OrderType.descending(),
+      ));
 
   NotesState get state => _state;
   Note? _recentlyDeletedNote;
@@ -25,7 +31,7 @@ class NotesViewModel extends ChangeNotifier {
   }
 
   Future<void> _loadNotes() async {
-    List<Note> notes = await _useCases.getNotesUseCase.call();
+    List<Note> notes = await _useCases.getNotesUseCase.call(noteOrder: state.noteOrder);
     _state = state.copyWith(notes: notes);
     notifyListeners();
   }

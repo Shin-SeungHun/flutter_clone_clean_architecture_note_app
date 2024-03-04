31559b98e6d7069feb84e1a6131fa8b5c172fd9f
shin-SeungHun <sng7387@gmail.com>
2024-03-04 18:13:28 +0900

    refactor: state 안에 NoteOrder 속성 추가

--- lib/presentation/notes/notes_state.dart
+++ lib/presentation/notes/notes_state.dart
@@ -1,15 +1,13 @@
 import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
+import 'package:flutter_clone_clean_architecture_note_app/domain/util/note_order.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'notes_state.freezed.dart';
 
-part 'notes_state.g.dart';
-
 @freezed
 class NotesState with _$NotesState {
-  const factory NotesState({
+  factory NotesState({
     required List<Note> notes,
+    required NoteOrder noteOrder,
   }) = _NotesState;
-
-  factory NotesState.fromJson(Map<String, Object?> json) => _$NotesStateFromJson(json);
 }

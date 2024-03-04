5196446deefc21d139e16d07bf02d869ee27b3f1
shin-SeungHun <sng7387@gmail.com>
2024-03-04 15:21:56 +0900

    style: 불필요한 import 제거

--- lib/presentation/notes/components/note_item.dart
+++ lib/presentation/notes/components/note_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_clone_clean_architecture_note_app/domain/model/note.dart';
 
 class NoteItem extends StatelessWidget {
